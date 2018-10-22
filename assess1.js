3c8adef05dc08eb882a55a3e69ac0fec7ffcde52
COR Students <corstudents@Matts-MBP-97.localdomain>
2018-10-22 12:16:06 -0400

    Finished Test

---
+++ assess1.js
@@ -0,0 +1,65 @@
+// // #1 Create Variable
+// let name = 'Baldhead'
+// let age = 28
+// let colorIsBlue = true
+// let pi = 3.14
+
+
+// // #2
+// let trueOrFalse  = "true"
+
+// if (trueOrFalse == "true") {
+//      console.log(1) 
+//      trueOrFalse = "false"    
+//     }
+//      else if (trueOrFalse == "false") {
+//         console.log(2)
+//     }
+     
+        
+         
+
+
+
+// #3
+
+ //for (let i = 0; i <= 100 ; i+=2) {
+       
+   // console.log([i])
+    
+ // }
+ // #4 
+//   let iAmBaldhead = true
+//  let counter = 0
+//   while (iAmBaldhead === true) {
+      
+      
+//       if (counter <= 20) {
+//          console.log('Hello')  
+//          counter ++ 
+         
+//      } else {
+//           iAmBaldhead = false
+//       }
+//   } 
+// // #5
+
+
+ function checkMe(canVote,) { 
+        canVote = 'true'
+     if (canVote == 'true')
+      {
+       
+        console.log('true')
+        canVote = 'false'
+     }
+     else
+     {
+          console.log('false')
+     }
+    }
+ checkMe();
+
+
+// Bonus 
+// Don't repeat yourself
